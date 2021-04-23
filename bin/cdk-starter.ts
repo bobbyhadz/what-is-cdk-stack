a029eea22aaf3b5401792b86643d48b352968886
Borislav Hadzhiev <opmftw@gmail.com>
2021-04-23 08:07:05 +0300

    initial commit

---
+++ bin/cdk-starter.ts
@@ -0,0 +1,25 @@
+#!/usr/bin/env node
+import * as cdk from '@aws-cdk/core';
+import {MyCdkStack} from '../lib/cdk-starter-stack';
+
+const app = new cdk.App();
+
+// create our DEV stack 👇
+new MyCdkStack(app, 'my-cdk-stack-dev', {
+  stackName: 'my-cdk-stack-dev',
+  env: {
+    region: process.env.CDK_DEFAULT_REGION,
+    account: process.env.CDK_DEFAULT_ACCOUNT,
+  },
+  tags: {env: 'dev'},
+});
+
+// create our PROD stack 👇
+new MyCdkStack(app, 'my-cdk-stack-prod', {
+  stackName: 'my-cdk-stack-prod',
+  env: {
+    region: process.env.CDK_DEFAULT_REGION,
+    account: process.env.CDK_DEFAULT_ACCOUNT,
+  },
+  tags: {env: 'prod'},
+});
---
+++ lib/cdk-starter-stack.ts
@@ -0,0 +1,16 @@
+import * as dynamodb from '@aws-cdk/aws-dynamodb';
+import * as cdk from '@aws-cdk/core';
+
+// define our Stack 👇
+export class MyCdkStack extends cdk.Stack {
+  constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // create a Dynamodb table using the Table construct
+    const table = new dynamodb.Table(this, 'my-table', {
+      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      partitionKey: {name: 'date', type: dynamodb.AttributeType.STRING},
+    });
+  }
+}
